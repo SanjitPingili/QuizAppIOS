dbacd70d4e2168fbbc9ce6183abd368c757ed970
nlalani8 <nlalani8@gatech.edu>
2022-10-12 00:31:44 -0400

    new
    new changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# ITS-Swift-Team-Fall-2022
-
-New Fall semester project to create the Tutoring Application using Swift for the IOS application
---
+++ newApp/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  newApp
+//
+//  Created by Neha Lalani on 9/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Welcome to TutorBot")
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+            }
+            .padding()
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
