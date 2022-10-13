f0bfd2a041e69d9dc7783c7892ef67b371c01fc6
Kerawala, Aahan <akerawala3@gatech.edu>
2022-10-13 11:41:07 -0400

    neww code
    updates for log in screen

--- newApp/ContentView.swift
+++ newApp/ContentView.swift
@@ -8,19 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Welcome to TutorBot")
-            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
-                
+        NavigationView {
+            ZStack {
+                Color.orange
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.4))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white.opacity(0.4))
+                Circle()
+                    .scale(1)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Welcome to TutorBot")
+                        .font(.title)
+                        .bold()
+                        .padding(.bottom, 2.0)
+                    
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    TextField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    Button("Login") {
+                        //Check if user exists
+                        checkUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.orange)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in @\(username)"), isActive: $showingLoginScreen) {
+                        EmptyView()
+                    }
+                }
             }
-            .padding()
+            .navigationBarHidden(true)
         }
-        .padding()
     }
+    func checkUser(username: String, password: String) {
+        //add check for usernames
+        if username.lowercased() == "test" {
+            wrongUsername = 0
+            if password.lowercased() == "password" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            } else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongUsername = 2
+        }
+    }
+        
+//        VStack {
+////            Image(systemName: "globe")
+////                .imageScale(.large)
+////                .foregroundColor(.accentColor)
+//            Text("Welcome to TutorBot")
+//                .padding(/*@START_MENU_TOKEN@*/.vertical, 5.0/*@END_MENU_TOKEN@*/)
+//            Text("Sign In!")
+//                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+//                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+//
+//        }
+//        .padding()
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
