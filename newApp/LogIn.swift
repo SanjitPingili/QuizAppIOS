0ba8ff30e12d6ed5db84040747dded93f886c4ce
nlalani8 <nlalani8@gatech.edu>
2022-10-31 14:13:31 -0400

    flash cards code no parameters
    new flashcard code

--- newApp/LogIn.swift
+++ newApp/LogIn.swift
@@ -246,58 +246,92 @@ struct Home: View {
 //flipping the flashcard code
 //implement to new page
 
-struct NewCards<Front, Back> : View where Front: View, Back: View {
-    var front: () -> Front
-    var back: () -> Back
+//struct NewCards<Front, Back> : View where Front: View, Back: View {
+//    var front: () -> Front
+//    var back: () -> Back
+//
+//    @State var flipped: Bool = false
+//    @State var flashcardRotation = 0.0
+//    @State var contentRotation = 0.0
+//
+//    init (@ViewBuilder front: @escaping () -> Front, @ViewBuilder back: @escaping () -> Back) {
+//        self.front = front
+//        self.back = back
+//    }
+//
+//    var body: some View {
+//        ZStack {
+//            if flipped {
+//                back()
+//
+//            } else {
+//                front()
+//            }
+//        }
+//        .rotation3DEffect(.degrees(contentRotation), axis: (x: 0, y: 1, z: 0))
+//        .padding()
+//        .frame(height: 200)
+//        .frame(maxWidth: .infinity)
+//        .background(Color.white)
+//        .overlay(
+//            Rectangle()
+//                .stroke(Color.black, lineWidth: 2)
+//
+//        )
+//        .padding()
+//        .onTapGesture {
+//            flipFlashcard()
+//        }
+//        .rotation3DEffect(.degrees(flashcardRotation), axis: (x: 0, y: 1, z: 0))
+//    }
+//
+//    func flipFlashcard() {
+//        let animationTime = 0.5
+//        withAnimation(Animation.linear(duration: 0.5)) {
+//            flashcardRotation += 180
+//            flipped.toggle()
+//
+//        }
+//        withAnimation(Animation.linear(duration: 0.001).delay(animationTime/2)) {
+//            contentRotation += 180
+//            flipped.toggle()
+//        }
+//    }
+//
+//
+//}
+//
 
-    @State var flipped: Bool = false
-    @State var flashcardRotation = 0.0
-    @State var contentRotation = 0.0
 
-    init (@ViewBuilder front: @escaping () -> Front, @ViewBuilder back: @escaping () -> Back) {
-        self.front = front
-        self.back = back
-    }
+//new flash cards implementation
 
+struct CardFront: View {
+    var degree : Double
+    let textContext : String
+    
     var body: some View {
         ZStack {
-            if flipped {
-                back()
-
-            } else {
-                front()
-            }
+            RoundedRectangle(cornerRadius: 20).stroke(.green.opacity(0.5), lineWidth: 10).padding()
+            
+            RoundedRectangle(cornerRadius: 20).stroke(.green.opacity(0.5), lineWidth: 10).padding()
+            
+            VStack {
+                Text("Question:")
+                    Text("New question 1")
+                
+                Text (textContext)
+                    .lineLimit(10)
+                    Text("answer here")
+        
         }
-        .rotation3DEffect(.degrees(contentRotation), axis: (x: 0, y: 1, z: 0))
-        .padding()
-        .frame(height: 200)
-        .frame(maxWidth: .infinity)
-        .background(Color.white)
-        .overlay(
-            Rectangle()
-                .stroke(Color.black, lineWidth: 2)
-
-        )
-        .padding()
-        .onTapGesture {
-            flipFlashcard()
-        }
-        .rotation3DEffect(.degrees(flashcardRotation), axis: (x: 0, y: 1, z: 0))
-    }
-    
-    func flipFlashcard() {
-        let animationTime = 0.5
-        withAnimation(Animation.linear(duration: 0.5)) {
-            flashcardRotation += 180
-            flipped.toggle()
-
-        }
-        withAnimation(Animation.linear(duration: 0.001).delay(animationTime/2)) {
-            contentRotation += 180
-            flipped.toggle()
+            
+            
         }
     }
-
-
 }
 
+struct CardFront_Previews : PreviewProvider {
+    static var previews: some View {
+        CardFront(degree: 0.0, textContext: "Question string goes here")
+    }
+}
