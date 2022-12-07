dbc4891da5d0404d9739b856f2beb6777c619db7
sajjan3 <sajjan3@gatech.edu>
2022-12-06 20:44:24 -0500

    small edits

--- newAppTests/newAppTests.swift
+++ newAppTests/newAppTests.swift
@@ -21,14 +21,14 @@ final class newAppTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test throws to produce an unexpected failure when your test encounters an error that is not caught.
+        // Mark your test async to allow awaiting for code that is asynchronous to complete. Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         measure {
-            // Put the code you want to measure the time of here.
+            // if you want to measure the time of code, put it here
         }
     }
 
