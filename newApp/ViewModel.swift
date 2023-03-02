06203dc115e4ba49ea401e526996722a24518fe4
Zhen Hong Tan <ztan95@gatech.edu>
2023-03-01 22:06:28 -0500

    updates

---
+++ newApp/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  newApp
+//
+//  Created by Zhen Hong Tan on 3/1/23.
+//
+
+import Foundation
---
+++ newApp/firebaseDB.swift
@@ -0,0 +1,8 @@
+//
+//  firebaseDB.swift
+//  newApp
+//
+//  Created by Zhen Hong Tan on 3/1/23.
+//
+
+import Foundation
