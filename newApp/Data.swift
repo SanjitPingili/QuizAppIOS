aeb0041057de9175eefed774cd67daabbfca335b
Parameswar, Samarth <samarth@gatech.edu>
2023-04-06 02:12:38 -0400

    SP:(Search Page and data)

---
+++ newApp/Data.swift
@@ -0,0 +1,12 @@
+//
+//  Data.swift
+//  newApp
+//
+//  Created by Samarth Parameswar on 4/6/23.
+//
+
+import Foundation
+
+public var Sets = ["ECE 2035 Test 1", "ECE 2035 Test 2", "Heap Management", "Software performance & reliability", "Data Structures", "Software performance & reliability", "Supporting Data Abstraction"]
+
+public var Questions = ["how long does both the inout and impulse response have?", "What does the block diagram tell us about an equivalent implemenation of hte system?", "what is an extension of the real number system?", "what are complex numbers necessary to solve?", "how many solutions does the previous equation have?", "what are numbers needed to solve for the two roots of a quadratic equation?", "whenever the discriminant is negative, the solution must be expressed as what?", "how many different mathematical notations can be used to represent complex numbers?", "what are the two basic types of polar form and rectangular form?", "converting between the two types quickly and easily is an important skill.", "rectangular form in rectangular form, all of the following notations define the same complex number?", "the ordered pair can be interpreted as what?", "a complex number can also be drawn as a vector whose tail is at the origin where?",]
