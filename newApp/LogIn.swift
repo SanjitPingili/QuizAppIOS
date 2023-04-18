37adecc9aa72dfb49028194705439b7695e696eb
Varun Patel <varunpatel@lawn-128-61-63-250.lawn.gatech.edu>
2023-04-18 18:01:41 -0400

    added seperate progress page to avoid coupling

--- newApp/LogIn.swift
+++ newApp/LogIn.swift
@@ -560,21 +560,7 @@ struct Quizzes: View {
             LogIn()
         }
     }
-struct Progress: View {
-    var body: some View {
-        VStack {
-            List {
-                Button("Quiz Summary (\(numQuestionsAnswered) answered)") {
-                    //Check if user exists
-                }
-                .foregroundColor(.black)
-                .frame(width: 300, height: 50)
-                .background(Color.orange)
-                .cornerRadius(10)
-            }
-        }
-    }
-}
+
 
 
 //new flash cards implementation
