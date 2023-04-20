d7c1cc2768f7d73d479c781824bb5872df62a363
Sahana Krishnan <sahanasr03@gmail.com>
2023-04-19 22:40:06 -0400

    fixed UI for version page

--- newApp/SavedQuestions.swift
+++ newApp/SavedQuestions.swift
@@ -3,38 +3,181 @@
 //  newApp
 //
 //  Created by Varun Patel on 4/18/23.
-//
+//  Updated by Sahana Krishnan on 4/19/23
 
 import Foundation
 import SwiftUI
+
 struct SavedQuestions: View {
     @State private var flag = false
+    @State private var flag1 = false
+    @State private var flag2 = false
+    @State private var flag3 = false
     var body: some View {
-        // NavigationView {
-        VStack {
-            List {
-                Button("Flagged Questions") {
-                    flag = true;
+        NavigationView {
+            VStack {
+                List {
+                    Button("Saved Questions") {
+                        flag = true;
+                    }
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+
+                    NavigationLink(destination: SavedView(), isActive: $flag) {
+                        EmptyView()
+                        
+                    }
+
+                    Button("Incorrect Questions") {
+                        flag1 = true;
+                    }
+                    .foregroundColor(.black)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    NavigationLink(destination: IncorrectView(), isActive: $flag1) {
+                        EmptyView()
+                        
+                    }
                 }
-                .foregroundColor(.black)
-                .frame(width: 300, height: 50)
-                .background(Color.blue)
-                .cornerRadius(10)
-                
-//                    NavigationLink(destination: SavedView(), isActive: $flag) {
-//                        EmptyView()
-//                    }
-                Button("Incorrect Questions") {
-                    //Check if user exists
-                }
-                .foregroundColor(.black)
-                .frame(width: 300, height: 50)
-                .background(Color.blue)
-                .cornerRadius(10)
             }
-        }.navigationTitle(Text("Saved Questions").font(.system(size: 56)))
-        
+            .navigationTitle(Text("Saved Questions").font(.system(size: 56)))
+        }
+    }
+}
+struct SavedView: View {
+    @State private var flag2 = false
+    @State private var flag3 = false
+    var body: some View {
+        VStack {
+            Text("Your saved questions")
+                .font(.headline)
+                .padding(.top, 20)
+            Button("Study Mode Saved questions") {
+                flag2 = true;
+            }
+            .foregroundColor(.black)
+            .frame(width: 300, height: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+
+            NavigationLink(destination: SavedView1(), isActive: $flag2) {
+                EmptyView()
+                
+            }
+            Text("")
+            Text("")
+            Text("")
+            Text("")
+            Text("")
+            
+            Button("Quiz Mode Saved questions") {
+                flag3 = true;
+            }
+            .foregroundColor(.black)
+            .frame(width: 300, height: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+
+            NavigationLink(destination: SavedView2(), isActive: $flag3) {
+                EmptyView()
+                
+            }
+        }
+        .navigationBarTitle("Saved", displayMode: .inline)
+    }
+}
+
+struct IncorrectView: View {
+    @State private var flag2 = false
+    @State private var flag3 = false
+    var body: some View {
+        VStack {
+            Text("Your incorrect questions")
+                .font(.headline)
+                .padding(.top, 20)
+            Button("Study Mode incorrect questions") {
+                flag2 = true;
+            }
+            .foregroundColor(.black)
+            .frame(width: 300, height: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+
+            NavigationLink(destination: SavedView3(), isActive: $flag2) {
+                EmptyView()
+                
+            }
+            Text("")
+            Text("")
+            Text("")
+            Text("")
+            Text("")
+            
+            Button("Quiz Mode incorrect questions") {
+                flag3 = true;
+            }
+            .foregroundColor(.black)
+            .frame(width: 300, height: 50)
+            .background(Color.blue)
+            .cornerRadius(10)
+
+            NavigationLink(destination: SavedView4(), isActive: $flag3) {
+                EmptyView()
+                
+            }
+        }
+        .navigationBarTitle("Incorrect", displayMode: .inline)
     }
 }
 
 
+struct SavedView1: View {
+    var body: some View {
+        VStack {
+            Text("Your Study Mode Saved questions")
+                .font(.headline)
+                .padding(.top, 20)
+            // add your saved questions list here
+        }
+        .navigationBarTitle("Saved", displayMode: .inline)
+    }
+}
+
+struct SavedView2: View {
+    var body: some View {
+        VStack {
+            Text("Your Quiz Mode Saved questions")
+                .font(.headline)
+                .padding(.top, 20)
+            // add your saved questions list here
+        }
+        .navigationBarTitle("Saved", displayMode: .inline)
+    }
+}
+
+struct SavedView3: View {
+    var body: some View {
+        VStack {
+            Text("Your Study Mode Incorrect questions")
+                .font(.headline)
+                .padding(.top, 20)
+            // add your saved questions list here
+        }
+        .navigationBarTitle("Saved", displayMode: .inline)
+    }
+}
+
+struct SavedView4: View {
+    var body: some View {
+        VStack {
+            Text("Your Quiz Mode Incorrect questions")
+                .font(.headline)
+                .padding(.top, 20)
+            // add your saved questions list here
+        }
+        .navigationBarTitle("Saved", displayMode: .inline)
+    }
+}
